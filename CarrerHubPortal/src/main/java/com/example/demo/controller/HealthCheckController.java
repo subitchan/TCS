503d6c609d5f4349cf92d14b032c345be8f1fa62
subitchan <74167727+subitchan@users.noreply.github.com>
2026-02-15 18:41:39 +0530

    Create HealthCheckController.java

---
+++ CarrerHubPortal/src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HealthCheckContoller {
+	@GetMapping("/health")
+	public String checkHealth()
+	{
+		return "Service is up";
+	}
+}
